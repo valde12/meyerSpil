d1eb5d86d06fe5c7a3124139665135da69b1aafd
Valdemar Ottesen <ottesenvaldemar@gmail.com>
2022-10-03 13:19:51 +0200

    test commit med terminal

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,10 @@ public class Main {
         TerningRaekke terningRaekke = new TerningRaekke();
         YatzyBlok yatzyBlok = new YatzyBlok();
         terningRaekke.slaaAlleTerninger();
+        for(int i : terningRaekke.returIntArray()) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
         terningRaekke.visGrafisk();
         yatzyBlok.visMulige(terningRaekke.returIntArray());
     }
--- src/test/java/YatzyBlok/YazyBlokFelter/Felter/Enkelt/EnereTest.java
+++ src/test/java/YatzyBlok/YazyBlokFelter/Felter/Enkelt/EnereTest.java
@@ -8,7 +8,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class EnereTest {
 
     static Enere testYatzyFelt;
-
+    static int[] TRUE_VAERDI_1 = new int[]{2,1,2,1,2,3};
+    static int[] TRUE_VAERDI_2 = new int[]{2,1,2,1,2};
+    static int[] FALSE_VAERDI_1 = new int[]{2,2,2,2,2,3};
+    static int[] FALSE_VAERDI_2 = new int[]{2,2,2,2,2};
+    static int RETURN_VAERDI_POINT = 2;
     @BeforeAll
     static void constructorTilFelt() {
         testYatzyFelt = new Enere("Navn");
@@ -16,40 +20,40 @@ class EnereTest {
 
     @Test
      void skalReturnTrue_erMulig() {
-        assertTrue(testYatzyFelt.erMulig(new int[]{2,1,2,1,2,3}));
-        assertTrue(testYatzyFelt.erMulig(new int[]{2,1,2,1,2}));
+        assertTrue(testYatzyFelt.erMulig(TRUE_VAERDI_1));
+        assertTrue(testYatzyFelt.erMulig(TRUE_VAERDI_2));
     }
 
     @Test
     void skalReturnFalse_erMulig() {
-        assertFalse(testYatzyFelt.erMulig(new int[]{2,2,2,2,2,3}));
-        assertFalse(testYatzyFelt.erMulig(new int[]{2,2,2,2,2}));
+        assertFalse(testYatzyFelt.erMulig(FALSE_VAERDI_1));
+        assertFalse(testYatzyFelt.erMulig(FALSE_VAERDI_2));
     }
 
     @Test
     void skalReturnFalseNaarBrugtPoint_erMulig() {
         testYatzyFelt.setPoint(12);
-        assertFalse(testYatzyFelt.erMulig(new int[]{2,1,2,1,2,3}));
-        assertFalse(testYatzyFelt.erMulig(new int[]{2,1,2,1,2}));
+        assertFalse(testYatzyFelt.erMulig(TRUE_VAERDI_1));
+        assertFalse(testYatzyFelt.erMulig(TRUE_VAERDI_2));
     }
 
     @Test
     void skalReturnFalseNaarBrugtTerninger_erMulig() {
-        testYatzyFelt.setTerninger(new int[]{1,1,2,2,2,3});
-        assertFalse(testYatzyFelt.erMulig(new int[]{1,1,2,2,2,3}));
-        assertFalse(testYatzyFelt.erMulig(new int[]{1,1,2,2,2}));
+        testYatzyFelt.setTerninger(TRUE_VAERDI_1);
+        assertFalse(testYatzyFelt.erMulig(TRUE_VAERDI_1));
+        assertFalse(testYatzyFelt.erMulig(TRUE_VAERDI_2));
     }
 
     @Test
     void skalReturn2Point_beregnPoint() {
-        assertEquals(2, testYatzyFelt.beregnPoint(new int[]{1,1,2,2,2,3}));
-        assertEquals(2, testYatzyFelt.beregnPoint(new int[]{1,1,2,2,2}));
+        assertEquals(RETURN_VAERDI_POINT, testYatzyFelt.beregnPoint(TRUE_VAERDI_1));
+        assertEquals(RETURN_VAERDI_POINT, testYatzyFelt.beregnPoint(TRUE_VAERDI_2));
     }
 
     @Test
     void skalReturn0Point_beregnPoint() {
-        assertEquals(0, testYatzyFelt.beregnPoint(new int[]{2,2,2,2,2,3}));
-        assertEquals(0, testYatzyFelt.beregnPoint(new int[]{2,2,2,2,2}));
+        assertEquals(0, testYatzyFelt.beregnPoint(FALSE_VAERDI_1));
+        assertEquals(0, testYatzyFelt.beregnPoint(FALSE_VAERDI_2));
         assertEquals(0, testYatzyFelt.beregnPoint(new int[]{}));
     }
 }

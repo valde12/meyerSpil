bc97ea3686adec277d678316b526a617051084de
Valdemar Ottesen <ottesenvaldemar@gmail.com>
2022-10-04 20:04:36 +0200

    Sat visMulige i egen class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,5 @@ public class Main {
         }
         System.out.println();
         terningRaekke.visGrafisk();
-        yatzyBlok.visMulige(terningRaekke.returIntArray());
     }
 }
--- src/main/java/YatzyBlok/YatzyBlok.java
+++ src/main/java/YatzyBlok/YatzyBlok.java
@@ -5,28 +5,27 @@ import YatzyBlok.YazyBlokFelter.Felter.Enkelt.*;
 import YatzyBlok.YazyBlokFelter.Felter.ParOgEns.*;
 import YatzyBlok.YazyBlokFelter.Felter.Special.*;
 import YatzyBlok.YazyBlokFelter.Felter.chance;
-import YatzyBlok.YazyBlokFelter.YatzyBlokFelt;
 
 public class YatzyBlok {
-    private int pointIAlt;
-    private final Enere enere;
-    private final Toere toere;
-    private final Treere treere;
-    private final Firer firer;
-    private final Femmere femmere;
-    private final Seksere seksere;
-    private final etPar etPar;
-    private final toPar toPar;
-    private final trePar trePar;
-    private final treEns treEns;
-    private final fireEns fireEns;
-    private final toGangeTreEns toGangeTreEns;
-    private final lilleStraight lilleStraight;
-    private final storStraight storStraight;
-    private final royal royal;
-    private final hus hus;
-    private final chance chance;
-    private final yatzy yatzy;
+    protected int pointIAlt;
+    protected final Enere enere;
+    protected final Toere toere;
+    protected final Treere treere;
+    protected final Firer firer;
+    protected final Femmere femmere;
+    protected final Seksere seksere;
+    protected final etPar etPar;
+    protected final toPar toPar;
+    protected final trePar trePar;
+    protected final treEns treEns;
+    protected final fireEns fireEns;
+    protected final toGangeTreEns toGangeTreEns;
+    protected final lilleStraight lilleStraight;
+    protected final storStraight storStraight;
+    protected final royal royal;
+    protected final hus hus;
+    protected final chance chance;
+    protected final yatzy yatzy;
 
     public YatzyBlok() {
         enere = new Enere("1'erne");
@@ -48,60 +47,6 @@ public class YatzyBlok {
         chance = new chance("Chance");
         yatzy = new yatzy("Yatzy");
         pointIAlt = 0;
-    }
-
-    public void visMulige(int[] terninger) {
-        int taeller = 0;
-        YatzyBlokFelt[] yatzyBlokFelter = {enere, toere, treere, firer, femmere, seksere, etPar, toPar, trePar, treEns, fireEns, toGangeTreEns, lilleStraight, storStraight, royal, hus, yatzy, chance};
-        for (YatzyBlokFelt x: yatzyBlokFelter) {
-            if (x.erMulig(terninger)) {
-                taeller++;
-            }
-        }
-        YatzyBlokFelt[] yatzyBlokFelterMulige = new YatzyBlokFelt[taeller];
-        int taeller2 = 0;
-        for (YatzyBlokFelt i : yatzyBlokFelter) {
-            if (i.erMulig(terninger)) {
-                yatzyBlokFelterMulige[taeller2] = i;
-                taeller2++;
-            }
-        }
-        for (YatzyBlokFelt i : yatzyBlokFelterMulige) {
-            System.out.println(i.seType() + " er mulig");
-            switch (i.seType()) {
-                case "1 par", "3 ens", "4 ens", "Yatzy" -> {
-                    for(int par : i.beregnMulige(terninger)){
-                        System.out.println(" - Med " + par + "'erne til " + i.beregnPointEns(par) + " point");
-                    }
-                }
-                case "2 par" -> {
-                    if (toPar.beregnMulige(terninger).length == 2) {
-                        System.out.println(" - Med " + toPar.beregnMulige(terninger)[0] + "'erne og " + toPar.beregnMulige(terninger)[1] + "'erne til " + toPar.beregnPoint(toPar.beregnMulige(terninger)[1], toPar.beregnMulige(terninger)[0]) + " point");
-                    } else {
-                        System.out.println(" - Med " + toPar.beregnMulige(terninger)[0] + "'erne og " + toPar.beregnMulige(terninger)[1] + "'erne til " + toPar.beregnPoint(toPar.beregnMulige(terninger)[1], toPar.beregnMulige(terninger)[0]) + " point");
-                        System.out.println(" - Med " + toPar.beregnMulige(terninger)[2] + "'erne og " + toPar.beregnMulige(terninger)[3] + "'erne til " + toPar.beregnPoint(toPar.beregnMulige(terninger)[2], toPar.beregnMulige(terninger)[3]) + " point");
-                        System.out.println(" - Med " + toPar.beregnMulige(terninger)[4] + "'erne og " + toPar.beregnMulige(terninger)[5] + "'erne til " + toPar.beregnPoint(toPar.beregnMulige(terninger)[4], toPar.beregnMulige(terninger)[5]) + " point");
-                    }
-                }
-                case "3 par" -> {
-                    System.out.println(" - Med " + trePar.beregnMulige(terninger)[0] + "'erne, " + trePar.beregnMulige(terninger)[1] + "'erne og " + trePar.beregnMulige(terninger)[2] + "'erne til " + trePar.beregnPoint(trePar.beregnMulige(terninger)[0], trePar.beregnMulige(terninger)[1], trePar.beregnMulige(terninger)[2]) + " point");
-                }
-                case "2x3 ens" -> {
-                    System.out.println(" - Med " + toGangeTreEns.beregnMulige(terninger)[0] + "'erne og " + toGangeTreEns.beregnMulige(terninger)[1] + "'erne til " + toGangeTreEns.beregnPoint(toGangeTreEns.beregnMulige(terninger)[0],toGangeTreEns.beregnMulige(terninger)[1]) + " point");
-                }
-                case "Hus" -> {
-                    if (hus.beregnMulige(terninger).length > 2) {
-                        System.out.println(" - Af 1 par med " + hus.beregnMulige(terninger)[0] + "'erne og 3 ens med " + hus.beregnMulige(terninger)[1] + "'erne til " + hus.beregnPoint(hus.beregnMulige(terninger)[0], hus.beregnMulige(terninger)[1]) + " point");
-                        System.out.println(" - Af 1 par med " + hus.beregnMulige(terninger)[2] + "'erne og 3 ens med " + hus.beregnMulige(terninger)[3] + "'erne til " + hus.beregnPoint(hus.beregnMulige(terninger)[2], hus.beregnMulige(terninger)[3]) + " point");
-                    } else {
-                        System.out.println(" - Af 1 par med " + hus.beregnMulige(terninger)[0] + "'erne og 3 ens med " + hus.beregnMulige(terninger)[1] + "'erne til " + hus.beregnPoint(hus.beregnMulige(terninger)[0], hus.beregnMulige(terninger)[1]) + " point");
-                    }
-                }
-                default -> {
-                    System.out.println(" - Til "+ i.beregnPoint(terninger) + " point");
-                }
-            }
-        }
     }
 
 
